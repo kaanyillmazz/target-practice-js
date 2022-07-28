7999eca671dce4e2408d7390079240d8e9d06f5c
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-07-28 22:01:32 +0300

    added sounds

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {increasePoints} from "./features/points/pointsSlice";
 
+let audio = new Audio('./assets/sounds/correct.mp3');
+
+
 function App() {
     const dispatch = useDispatch();
     const points = useSelector((state) => state.points.pointsCount);
 
     const myLogic = (event) => {
+        audio.currentTime = 0;
+        audio.play();
         event.target.style.opacity = 0;
         dispatch(increasePoints())
         setTimeout(()=>{event.target.style.opacity = 100;},1000);
@@ -17,11 +22,12 @@ function App() {
 
   return (
     <div className="App">
-    <label style={{fontSize: "large"}}>{points} Points</label>
+    <label style={{fontSize: "50px"}}>{points} Points</label>
      <div className="imageHolder">
-         <img onClick={myLogic} className="mainImage" src={'./assets/images/bullseye.png'} />
-         <img onClick={myLogic} className="mainImage0" src={'./assets/images/bullseye.png'} />
-         <img onClick={myLogic} className="mainImage1" src={'./assets/images/bullseye.png'} />
+         <img draggable="false" onClick={myLogic} className="mainImage" src={'./assets/images/bullseye.png'} />
+         <img draggable="false" onClick={myLogic} className="mainImage0" src={'./assets/images/bullseye.png'} />
+         <img draggable="false" onClick={myLogic} className="mainImage1" src={'./assets/images/bullseye.png'} />
+         <img draggable="false" onClick={myLogic} className="mainImage2" src={'./assets/images/bullseye.png'} />
      </div>
     </div>
   );
