16a6bc4e639ac612c3b160d430712aad03cc236c
kyilmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-08-01 14:25:42 +0300

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -2,35 +2,117 @@ import logo from './logo.svg';
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {increasePoints} from "./features/points/pointsSlice";
+import {decreaseCount} from "./features/points/countSlice";
 
 let audio = new Audio('./assets/sounds/correct.mp3');
 
 
 function App() {
+
+
     const dispatch = useDispatch();
     const points = useSelector((state) => state.points.pointsCount);
+    const count = useSelector((state) => state.count.value);
 
     const myLogic = (event) => {
         audio.currentTime = 0;
         audio.play();
         event.target.classList.add("disabled");
         dispatch(increasePoints())
-        setTimeout(()=>{event.target.classList.remove("disabled")},1000);
+        setTimeout(() => {
+            event.target.classList.remove("disabled")
+        }, 1000);
+
+    }
+
+    let countLocal = 60;
+
+    function setTime() {
+        countLocal--;
+        let counterLabel = document.getElementById("counterLabel");
+        counterLabel.innerText = countLocal.toString() + " Seconds Remaining!";
+    }
+
+
+    const startGame = (event) => {
+        let b1 = document.getElementById("b1");
+        let b2 = document.getElementById("b2");
+        let b3 = document.getElementById("b3");
+        let b4 = document.getElementById("b4");
+        let myLabel = document.getElementById("countdown");
+        myLabel.classList.add("countAnim");
+        setTimeout(() => {
+            myLabel.innerText = "2"
+        }, 1000);
+        setTimeout(() => {
+            myLabel.innerText = "1"
+        }, 2000);
+        setTimeout(() => {
+            myLabel.innerText = "GO"
+        }, 3000);
+        setTimeout(() => {
+            b1.classList.remove("disabled")
+        }, 3000);
+        setTimeout(() => {
+            b2.classList.remove("disabled")
+        }, 3000);
+        setTimeout(() => {
+            b3.classList.remove("disabled")
+        }, 3000);
+        setTimeout(() => {
+            b4.classList.remove("disabled")
+        }, 3000);
+        let myVar;
+        setTimeout(() => {
+            myVar = setInterval(() => {
+                setTime()
+            }, 1000);
+        }, 3000);
+        setTimeout(() => {
+            clearInterval(myVar);
+        }, 64000);
+        setTimeout(() => {
+            b1.classList.add("disabled")
+        }, 64000);
+        setTimeout(() => {
+            b2.classList.add("disabled")
+        }, 64000);
+        setTimeout(() => {
+            b3.classList.add("disabled")
+        }, 64000);
+        setTimeout(() => {
+            b4.classList.add("disabled")
+        }, 64000);
+        setTimeout(() => {
+            myLabel.innerText = "Time's up!"
+        }, 64000);
 
     }
 
 
-  return (
-    <div className="App">
-    <label style={{fontSize: "50px"}}>{points} Points</label>
-     <div className="imageHolder">
-         <img draggable="false" onClick={myLogic} className="mainImage" src={'./assets/images/bullseye.png'} />
-         <img draggable="false" onClick={myLogic} className="mainImage0" src={'./assets/images/bullseye.png'} />
-         <img draggable="false" onClick={myLogic} className="mainImage1" src={'./assets/images/bullseye.png'} />
-         <img draggable="false" onClick={myLogic} className="mainImage2" src={'./assets/images/bullseye.png'} />
-     </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="info">
+                <label style={{fontSize: "50px"}}>{points} Points</label>
+                <label id="counterLabel" style={{fontSize: "50px"}}>60 Seconds Remaining</label>
+                <button onClick={startGame}>Start</button>
+            </div>
+            <div className="count">
+                <label className="countLabel" id="countdown">3</label>
+            </div>
+
+            <div className="imageHolder">
+                <img id="b1" draggable="false" onClick={myLogic} className="mainImage disabled"
+                     src={'./assets/images/bullseye.png'}/>
+                <img id="b2" draggable="false" onClick={myLogic} className="mainImage0 disabled"
+                     src={'./assets/images/bullseye.png'}/>
+                <img id="b3" draggable="false" onClick={myLogic} className="mainImage1 disabled"
+                     src={'./assets/images/bullseye.png'}/>
+                <img id="b4" draggable="false" onClick={myLogic} className="mainImage2 disabled"
+                     src={'./assets/images/bullseye.png'}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import pointsSlice from "../features/points/pointsSlice";
+import countSlice from "../features/points/countSlice";
 
 export const store = configureStore({
     reducer: {
-        points: pointsSlice
+        points: pointsSlice,
+        count: countSlice
     },
 })
